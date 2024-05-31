55c7d303087c7a486ffa161149ddbb190e5eabe6
Welinton <welinton.haas@neomind.com.br>
2024-05-31 14:43:54 -0300

    Módulo 13: Criptografando a Senha e Testando o Fluxo de Cadastro de Usuário e Permissões

--- src/main/java/io/github/secutiry/domain/service/UsuarioService.java
+++ src/main/java/io/github/secutiry/domain/service/UsuarioService.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,10 +21,13 @@ public class UsuarioService
 	private final UsuarioRepository usuarioRepository;
 	private final GrupoRepository grupoRepository;
 	private final UsuarioGrupoRepository usuarioGrupoRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	@Transactional
 	public Usuario salvar(Usuario usuario, List<String> grupos)
 	{
+		String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaCriptografada);
 		usuarioRepository.save(usuario);
 
 		List<UsuarioGrupo> listaUsuarioGrupo = grupos.stream().map(nomeGrupo -> {
