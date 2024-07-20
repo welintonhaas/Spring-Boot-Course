0f48e95d72d7598329f6d29a70668ee7e2d5279d
Welinton <welinton.haas@neomind.com.br>
2024-07-20 20:26:41 -0300

    feat(Arquivo de Configuração): Trabalhando com multiplos ambientes e profiles

--- src/main/java/org/example/cursospringboot/MinhaConfiguration.java
+++ src/main/java/org/example/cursospringboot/MinhaConfiguration.java
@@ -1,10 +1,18 @@
 package org.example.cursospringboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration
 {
-
+	@Bean
+	public CommandLineRunner executar(){
+		return args -> {
+			System.out.println("RODANDO AMBIENTE DE DESENVOLVIMENTO");
+		};
+	}
 }
