5a6d81965fb6844754b8f9747ea6dfa0f30e1638
Welinton <welinton.haas@neomind.com.br>
2024-07-20 20:34:35 -0300

    feat(Arquivo de Configuração): Criando annotations customizadas de configuração

--- src/main/java/org/example/cursospringboot/MinhaConfiguration.java
+++ src/main/java/org/example/cursospringboot/MinhaConfiguration.java
@@ -2,11 +2,8 @@ package org.example.cursospringboot;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MinhaConfiguration
 {
 	@Bean
