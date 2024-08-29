4379316f5ac7f3d140b7f6a2e0e40618a378c353
Welinton Haas <welinton.haas@gmail.com>
2024-08-29 19:31:44 -0300

    Mapeando a Entidade Pedido

--- src/main/java/org/example/cursospringboot/domain/entity/Produto.java
+++ src/main/java/org/example/cursospringboot/domain/entity/Produto.java
@@ -1,11 +1,20 @@
 package org.example.cursospringboot.domain.entity;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
 public class Produto
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+
+	@Column(name = "descricao")
 	private String descricao;
+
+	@Column(name = "preco_unitario")
 	private BigDecimal preco;
 
 	public Integer getId()
